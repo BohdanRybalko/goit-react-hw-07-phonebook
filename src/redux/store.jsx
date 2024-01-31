ef827bbdd2ffebd69e504f58b46d7f737ccedae9
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2024-01-31 12:14:31 +0200

    fix store and contactApi

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,26 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import contactsReducer from './contactsSlice';
-import logger from 'redux-logger';
-
-const ignoreActions = ['FLUSH', 'REHYDRATE', 'PAUSE', 'PERSIST', 'PURGE', 'REGISTER'];
-
-const middleware = (getDefaultMiddleware) => {
-  const middlewares = getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: ignoreActions.map((actionType) => `persist/${actionType}`),
-    },
-  });
-
-  if (process.env.NODE_ENV === 'development') {
-    middlewares.push(logger);
-  }
-
-  return middlewares;
-};
 
 const store = configureStore({
   reducer: contactsReducer,
-  middleware,
+  
 });
 
 export default store;
